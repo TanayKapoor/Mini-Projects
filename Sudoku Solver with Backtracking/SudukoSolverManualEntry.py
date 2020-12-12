1966cf31f69890a3a0eb7cb4a9b1b54d4e504d63
tanaykapoor <itstkaay@gmail.com>
2020-12-12 15:11:34 +0530

    Added base algorithm to SudokuSlover

---
+++ Sudoku Solver with Backtracking/SudukoSolverManualEntry.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+grid = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
+
+
+def possible(x, y, n):
+    for i in range(0, 9):
+        if grid[i][x] == n and i != y:
+            return False
+
+    for i in range(0, 9):
+        if grid[y][i] == n and i != x:
+            return False
+
+    x0 = (x // 3) * 3
+    y0 = (y // 3) * 3
+    for X in range(x0, x0 + 3):
+        for Y in range(y0, y0 + 3):
+            if grid[Y][X] == n:
+                return False
+    return True
+
